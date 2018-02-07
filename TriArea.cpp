a788d23a213295db8814d7ce4b75dd145f346439
carlparte <carlparte@yahoo.com>
2018-02-07 10:54:56 +0800

    Modify to use pow() function.

--- TriArea.cpp
+++ TriArea.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
     double a, b, c;
-    double s, A;
+    double s, A, B;
     cout << "Input three edges a,b,c= ";
     cin >> a >> b >> c;
     s = (a + b + c) / 2;
     A = sqrt(s * (s - a) * (s - b) * (s - c));
-    cout << "The area is " << A << endl;
+    B = pow(s * (s - a) * (s - b) * (s - c), 0.5);
+    cout << "The area is " << A << endl << B << endl;
     return 0;
 }
