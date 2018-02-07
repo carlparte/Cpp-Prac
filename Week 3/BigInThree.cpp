7084faab4be89e794fb35717bb57a29fae475cc8
carlparte <carlparte@yahoo.com>
2018-02-07 13:00:41 +0800

    Modify to improve efficiency.

--- Week 3/BigInThree.cpp
+++ Week 3/BigInThree.cpp
@@ -5,19 +5,27 @@ int main()
 {
     double a, b, c, max;
     cin >> a >> b >> c;
-    if(a>b)
+    // if(a>b)
+    // {
+    //     if(a>c)
+    //         max = a;
+    //     else
+    //         max = c;
+    // }
+    // else
+    // {
+    //     if(b<c)
+    //         max = c;
+    //     else
+    //         max = b;
+    // }
+    max = a;
+    if(!(a>b&&a>c))
     {
-        if(a>c)
-            max = a;
-        else
-            max = c;
-    }
-    else
-    {
-        if(b<c)
-            max = c;
-        else
+        if(b>c)
             max = b;
+        else
+            max = c;
     }
     cout << max << endl;
     return 0;
