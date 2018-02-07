2ce34fdd53e3728dac5820ac2aab09bbd28df989
carlparte <carlparte@yahoo.com>
2018-02-07 23:14:22 +0800

    Add program CalcE. 用无穷级数求e的近似值。

---
+++ Week 3/CalcE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double e = 1.0, u = 1.0;
+    int n = 1;
+    while(u>=1.0E-7)
+    {
+        u = u / n;
+        e = e + u;
+        n = n + 1;
+    }
+    cout << "e=" << e << "(n=" << n << ")" << endl;
+    return 0;
+}
